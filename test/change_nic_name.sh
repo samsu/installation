c981edc55f9d3bb3939756d08eba10a74d3d113b
samsu <susltd.su@gmail.com>
2016-05-12 14:55:42 -0700

    rename nic interface name and assign a static ip address

--- test/change_nic_name.sh
+++ test/change_nic_name.sh
@@ -1,41 +1,77 @@
 #!/bin/bash
 
-#hostname="centos7-1"
+# example hostname="centos7-1"
 hostname=""
-nic_cur_names="eno16777728 eno33554952 eno50332176"
-nic_names="eth0 eth1 eth2"
 
-nic_names=${nic_names:-'eth0'}
+# The element format in the array NIC_NAME_MAP:
+# current_nic_name = new_nic_name [ip] [netmask] [gateway]
+declare -A NIC_NAME_MAP=(
+    [eno16777728]="eth0 10.160.37.61 255.255.255.0 10.160.37.1"
+    [eno33554952]="eth1 192.168.100.61 255.255.255.0"
+    [eno50332176]="eth2"
+)
+
+NIC_CONF_PATH="/etc/sysconfig/network-scripts"
+NIC_CONF_FILE_PREFIX="$NIC_CONF_PATH/ifcfg"
+
+[ -d "$NIC_CONF_PATH" ] || exit $?
 
 if [ ! -z "$hostname" ]; then
     hostnamectl set-hostname "$hostname"
 fi
 
-# set proxy
-#cat > /etc/environment << EOF
-#http_proxy=http://172.30.240.5:3128
-#https_proxy=http://172.30.240.5:3128
-#EOF
-#source /etc/environment
-
 # enable the command ifconfig
-yum update -y
-yum install -y net-tools tcpdump vim
+#yum update -y
+#yum install -y net-tools tcpdump vim
+
+set -o xtrace
+
+# change nic naming style from enoxxxxx to ethxx
+for NIC in ${!NIC_NAME_MAP[@]}; do
+    read -r -a NIC_INFO <<< "${NIC_NAME_MAP[$NIC]}"
+    NEW_NIC=${NIC_INFO[0]}
+
+    if [ ! -z $NEW_NIC ]; then
+        MAC=$(cat /sys/class/net/$NIC/address) || exit $?
+        cat >> $NIC_CONF_FILE_PREFIX-$NEW_NIC < EOF
+TYPE=Ethernet
+BOOTPROTO=none
+DEFROUTE=yes
+PEERDNS=yes
+PEERROUTES=yes
+IPV4_FAILURE_FATAL=no
+NAME=$NEW_NIC
+DEVICE=$NEW_NIC
+HWADDR=$MAC
+ONBOOT=yes
+EOF
+        if [ ! -z "${NIC_INFO[1]}" ]; then
+            IPADDR="${NIC_INFO[1]}"
+            cat >> $NIC_CONF_FILE_PREFIX-$NEW_NIC << EOF
+IPADDR=$IPADDR
+EOF
+        fi
+
+        if [ ! -z "${NIC_INFO[2]}" ]; then
+            NETMASK="${NIC_INFO[2]}"
+            cat >> $NIC_CONF_FILE_PREFIX-$NEW_NIC << EOF
+NETMASK=$NETMASK
+EOF
+        fi
+
+        if [ ! -z "${NIC_INFO[3]}" ]; then
+            GATEWAY="${NIC_INFO[3]}"
+            cat >> $NIC_CONF_FILE_PREFIX-$NEW_NIC << EOF
+GATEWAY=$GATEWAY
+EOF
+        fi
+
+        if [ -e "$NIC_CONF_FILE_PREFIX-$NIC" ]; then
+            rm -rf $NIC_CONF_FILE_PREFIX-$NIC
+        fi
+    fi
+done
 
 # disable the predictable naming rule
 sed -i 's#GRUB_CMDLINE_LINUX="rd.lvm.lv=centos/root#GRUB_CMDLINE_LINUX="net.ifnames=0 rd.lvm.lv=centos/root#g' /etc/default/grub
 grub2-mkconfig -o /boot/grub2/grub.cfg
-
-# change nic naming style from enoxxxxx to ethxx
-i=1
-IFS=" "
-set -- $nic_names
-for nic_cur_name in $nic_cur_names; do
-    nic_name=${!i}
-    if [ -e "/etc/sysconfig/network-scripts/ifcfg-$nic_cur_name" ] && [ ! -e "/etc/sysconfig/network-scripts/ifcfg-$nic_name" ]; then
-        mv "/etc/sysconfig/network-scripts/ifcfg-$nic_cur_name" "/etc/sysconfig/network-scripts/ifcfg-$nic_name"
-    fi
-    sed -i "s#$nic_cur_name#$nic_name#g" "/etc/sysconfig/network-scripts/ifcfg-$nic_name"
-    ((i+=1))
-done
-
