23c7ceaaf44b3a1a43b93d0492eae31decfccc27
samsu <susltd.su@gmail.com>
2017-04-26 17:09:56 -0700

    add a service check function

--- conf/ocata/horizon_config.sh
+++ conf/ocata/horizon_config.sh
@@ -1,7 +1,13 @@
 function _horizon_configure() {
-    sed -i.bak "s#OPENSTACK_HOST = \"127.0.0.1\"#OPENSTACK_HOST = \"$CTRL_MGMT_IP\"#g" /etc/openstack-dashboard/local_settings
-    sed -i "s#ALLOWED_HOSTS = \['horizon.example.com', 'localhost'\]#ALLOWED_HOSTS = \['*', \]#g" /etc/openstack-dashboard/local_settings
-    sed -i "s#'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',#'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',\n\t'LOCATION': '127.0.0.1:11211',#g" /etc/openstack-dashboard/local_settings
+    crudini --set /etc/openstack-dashboard/local_settings '' OPENSTACK_HOST "\"$CTRL_MGMT_IP\""
+    crudini --set /etc/openstack-dashboard/local_settings '' ALLOWED_HOSTS "['*', ]"
+    crudini --set /etc/openstack-dashboard/local_settings '' CACHES "{
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': '$CTRL_MGMT_IP:11211',
+    },
+}"
+    crudini --set local_settings '' OPENSTACK_KEYSTONE_URL '"http://%s:5000/v3" % OPENSTACK_HOST'
     sed -i "s/^#OPENSTACK_API_VERSIONS = {/OPENSTACK_API_VERSIONS = {/g" /etc/openstack-dashboard/local_settings
     sed -i 's/^#    "identity": 3,/     "identity": 3,/g' /etc/openstack-dashboard/local_settings
     sed -i "s/^#    \"volume\": 2,/     \"volume\": 2,\n}/g" /etc/openstack-dashboard/local_settings
