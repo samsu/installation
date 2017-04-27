9cfd4ed7963f990c869032e51a687f90a8cc14f9
samsu <susltd.su@gmail.com>
2017-04-26 17:26:05 -0700

    add a service check function

--- conf/ocata/horizon_config.sh
+++ conf/ocata/horizon_config.sh
@@ -5,8 +5,8 @@ function _horizon_configure() {
     'default': {
         'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
         'LOCATION': '$CTRL_MGMT_IP:11211',
-    },
-}"
+    },"
+
     crudini --set local_settings '' OPENSTACK_KEYSTONE_URL '"http://%s:5000/v3" % OPENSTACK_HOST'
     sed -i "s/^#OPENSTACK_API_VERSIONS = {/OPENSTACK_API_VERSIONS = {/g" /etc/openstack-dashboard/local_settings
     sed -i 's/^#    "identity": 3,/     "identity": 3,/g' /etc/openstack-dashboard/local_settings
