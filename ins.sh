106e4bf75c40343c886cb08a1975bb690575d39f
samsu <susltd.su@gmail.com>
2017-04-28 09:14:15 -0700

    add a service check function

--- ins.sh
+++ ins.sh
@@ -5,7 +5,7 @@
 # eth0=$(ip address show eth0 | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')
 # centos use
 # export TOP_DIR=$(cd $(dirname "$0") && pwd)
-[[ -n $TOP_DIR ]] || export TOP_DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
+[[ -n $TOP_DIR ]] || export TOP_DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
 
 source "$TOP_DIR/local.conf"
 source "$TOP_DIR/common/parameters.sh"
