d70d9495f4262f8ba1a7d2a0a74e235ceffc5d66
Sam Su <sansu@fortinet.com>
2018-01-10 18:19:58 -0800

    add a service check function

--- conf/mitaka/db_creation.sh
+++ conf/mitaka/db_creation.sh
@@ -36,6 +36,7 @@ function _services_db_creation() {
         if [[ ${service,,} == 'nova' ]]; then
             _db_creation $SERVICE_DB_USER $SERVICE_DB_PWD nova_api
         fi
+        echo "The database for ${service} was created."
     done
 }
 
