578d1418a9bfcee0b29f2b37301be18db02ee2a2
Jerry Zhao <jerryzhao@fortinet.com>
2018-06-13 21:27:46 -0700

    use load balancer ip for memcached service
    so that dashboard can be load balanced to multiple nodes.
    memcached still uses first live node

--- conf/ocata/horizon_config.sh
+++ conf/ocata/horizon_config.sh
@@ -2,7 +2,7 @@ function _horizon_configure() {
     _CONF='/etc/openstack-dashboard/local_settings'
     crudini --set $_CONF '' OPENSTACK_HOST "\"$CTRL_MGMT_IP\""
     crudini --set $_CONF '' ALLOWED_HOSTS "['*', ]"
-    sed -i "s#'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',#'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',\n\t'LOCATION': '127.0.0.1:11211',#g" $_CONF
+    sed -i "s#'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',#'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',\n\t'LOCATION': '$CTRL_MGMT_IP:11211',#g" $_CONF
     crudini --set $_CONF '' OPENSTACK_KEYSTONE_URL '"http://%s:5000/v3" % OPENSTACK_HOST'
     sed -i "s/^#OPENSTACK_API_VERSIONS = {/OPENSTACK_API_VERSIONS = {/g" $_CONF
     sed -i 's/^#    "identity": 3,/     "identity": 3,/g' $_CONF
